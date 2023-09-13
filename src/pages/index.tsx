00706f5a8cff7359f071381669fcf48d4bb9c10d
Farid Mansimli <f.mensimli@asan.gov.az>
2023-09-13 17:28:42 +0400

    nextjs-example (0.1.0) ## çərşənbə, 13 sentyabr 2023 17:28:42 +04 ##

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Inter } from "next/font/google";
+import { useLocale } from "@/hooks";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { t } = useLocale();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <div>Home Page...</div>
+      <div>{t("home-page")}...</div>
     </div>
   );
 }
