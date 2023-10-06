2f4a78df203e764b8e6e2bb387ca79029e6d2eb9
Farid Mansimli <f.mensimli@asan.gov.az>
2023-10-06 16:33:44 +0400

    nextjs-example (0.1.0) ## cümə, 06 oktyabr 2023 16:33:44 +04 ##

--- src/swr/fetcher.ts
+++ src/swr/fetcher.ts
@@ -1,25 +1,3 @@
 import { http } from "@/http/http";
 
 export const fetcher = (url: string) => http.get(url).then((res) => res.data);
-
-// import type { AxiosRequestConfig } from "axios";
-// import { http } from "@/http/http";
-
-// interface IConfig {
-//   url: string;
-//   baseURL: string;
-//   method: "get" | "post" | "put" | "delete" | "patch" | "options";
-// }
-
-// export const fetcher = (key: string | IConfig) => {
-//   if (typeof key === "string") {
-//     return http.get(key).then((res) => res.data);
-//   }
-
-//   return async (key: IConfig, { arg }: { arg: AxiosRequestConfig }) => {
-//     return http[key.method || "get"](key.url, {
-//       [key.baseURL ? "baseURL" : ""]: key.baseURL,
-//       ...arg
-//     }).then((res) => res.data);
-//   };
-// };
