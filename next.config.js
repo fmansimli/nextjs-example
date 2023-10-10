f7f41245b10d0d537fc7f34344506c5753cd1b4b
Farid Mansimli <f.mensimli@asan.gov.az>
2023-10-10 15:12:44 +0400

    nextjs-example (0.1.0) ## çərşənbə axşamı, 10 oktyabr 2023 15:12:44 +04 ##

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     locales: ["az", "en", "ru"],
     defaultLocale: "az",
     localeDetection: false
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**"
+      }
+    ]
   }
 };
 
